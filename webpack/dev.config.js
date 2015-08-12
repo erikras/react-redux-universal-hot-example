61e5e6dc30a43f1e4afa30f3f0f86786d5f701a0
Erik Rasmussen <erik.rasmussen@inetalliance.net>
2015-08-12 10:37:09 +0200

    upgraded redux-devtools to 0.2.0

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -47,7 +47,7 @@ module.exports = {
       __CLIENT__: true,
       __SERVER__: false,
       __DEVELOPMENT__: true,
-      __DEVTOOLS__: false  // <-------- DISABLE redux-devtools HERE
+      __DEVTOOLS__: true  // <-------- DISABLE redux-devtools HERE
     }),
 
     // stats
