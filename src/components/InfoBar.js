332734ed6bda3325b676b2c9f864cc05b308425b
L <lilei.leon@gmail.com>
2015-08-13 17:57:11 +0800

    mocha play

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -2,9 +2,9 @@ import React, {Component, PropTypes} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {load} from '../actions/infoActions';
-import {requireServerCss} from '../util';
+//import {requireServerCss} from '../util';
 
-const styles = __CLIENT__ ? require('./InfoBar.scss') : requireServerCss(require.resolve('./InfoBar.scss'));
+//const styles = __CLIENT__ ? require('./InfoBar.scss') : requireServerCss(require.resolve('./InfoBar.scss'));
 
 @connect(
     state => ({info: state.info.data}),
@@ -18,16 +18,15 @@ export default class InfoBar extends Component {
   render() {
     const {info, load} = this.props; // eslint-disable-line no-shadow
     return (
-      <div className={styles.infoBar + ' well'}>
+      <div className={' well'}>
         <div className="container">
           This is an info bar
           {' '}
           <strong>{info ? info.message : 'no info!'}</strong>
-          <span className={styles.time}>{info && new Date(info.time).toString()}</span>
+          <span>{info && new Date(info.time).toString()}</span>
           <button className="btn btn-primary" onClick={load}>Reload from server</button>
         </div>
       </div>
     );
   }
 }
-
