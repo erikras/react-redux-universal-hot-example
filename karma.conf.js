9a5794d5ac434ccb1b6160831467943a29bce59b
L <lilei.leon@gmail.com>
2015-08-14 21:16:26 +0800

    should enable single run to pass the build on travis-ci

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function (config) {
 
     browsers: [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ],
 
-    singleRun: false,
+    singleRun: true,
 
     frameworks: [ 'mocha' ],
 
