ee419afbfe789dd8a16dec258b4bfd4c9f50e675
Erik Rasmussen <rasmussenerik@gmail.com>
2015-08-18 17:45:34 +0200

    fixed typo in comment

--- src/validation/surveyValidation.js
+++ src/validation/surveyValidation.js
@@ -4,6 +4,6 @@ import {createValidator, required, maxLength, email} from './validation';
 const surveyValidation = createValidator({
   name: [required, maxLength(10)],
   email: [required, email],
-  occupation: maxLength(20) // single rules don't have to in an array
+  occupation: maxLength(20) // single rules don't have to be in an array
 });
 export default memoize(10)(surveyValidation);
