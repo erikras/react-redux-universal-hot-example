c7e9deafa005dac17ba30fdb1d310af1101d040e
Erik Rasmussen <rasmussenerik@gmail.com>
2015-09-02 22:44:27 +0200

    rolled back last commit which prematurely tried to upgrade to "history" lib

--- src/client.js
+++ src/client.js
@@ -3,13 +3,13 @@
  */
 import 'babel/polyfill';
 import React from 'react';
-import createHistory from 'history/lib/createBrowserHistory';
+import BrowserHistory from 'react-router/lib/BrowserHistory';
 import Location from 'react-router/lib/Location';
 import queryString from 'query-string';
 import createStore from './redux/create';
 import ApiClient from './ApiClient';
 import universalRouter from './universalRouter';
-const history = createHistory();
+const history = new BrowserHistory();
 const client = new ApiClient();
 
 const dest = document.getElementById('content');
