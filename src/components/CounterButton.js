91a376b744942d607e0aba8a75e862cbbc09e3e5
Buck DeFore <bdefore@gmail.com>
2015-09-08 11:37:43 +0200

    add eslint-loader for linting per webpack dev build

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -13,6 +13,10 @@ export default class CounterButton extends Component {
     className: PropTypes.string
   }
 
+  props = {
+    className: ''
+  }
+
   render() {
     const {count, increment} = this.props; // eslint-disable-line no-shadow
     let {className} = this.props;
@@ -22,10 +26,6 @@ export default class CounterButton extends Component {
         You have clicked me {count} time{count === 1 ? '' : 's'}.
       </button>
     );
-  }
-
-  props = {
-    className: ''
   }
 }
 
