91cfa1e61147517e11ff258f5702383d98fb1077
stevo <stevoland@gmail.com>
2015-09-15 19:13:54 +0100

    Replaced react-hot-loader with webpack-hot-middleware
    Hot loading remains in a separate process to allow server reloading
    
    Also unifies babel configs in .babelrc

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -11,7 +11,7 @@ var assetsPath = path.join(__dirname, relativeAssetsPath);
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
 var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
-var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'))
+var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
 
 module.exports = {
   devtool: 'source-map',
@@ -27,7 +27,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: [strip.loader('debug'), 'babel?stage=0&optional=runtime&plugins=typecheck']},
+      { test: /\.js$/, exclude: /node_modules/, loaders: [strip.loader('debug'), 'babel']},
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap=true&sourceMapContents=true') },
       { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
--- webpack/webpack-dev-server.js
+++ webpack/webpack-dev-server.js
@@ -1,21 +1,33 @@
-var WebpackDevServer = require('webpack-dev-server'),
-  webpack = require('webpack'),
-  config = require('./dev.config'),
-  host = process.env.HOST || 'localhost',
-  port = parseInt(process.env.PORT) + 1 || 3001,
-  serverOptions = {
-    contentBase: 'http://' + host + ':' + port,
-    quiet: true,
-    noInfo: true,
-    hot: true,
-    inline: true,
-    lazy: false,
-    publicPath: config.output.publicPath,
-    headers: {"Access-Control-Allow-Origin": "*"},
-    stats: {colors: true}
-  },
-  webpackDevServer = new WebpackDevServer(webpack(config), serverOptions);
+var Express = require('express');
+var webpack = require('webpack');
 
-webpackDevServer.listen(port, host, function() {
-  console.info('==> 🚧  Webpack development server listening on %s:%s', host, port);
+var config = require('../src/config');
+var webpackConfig = require('./dev.config');
+var compiler = webpack(webpackConfig);
+
+var host = process.env.HOST || 'localhost';
+var port = parseInt(config.port, 10) + 1 || 3001;
+var serverOptions = {
+  contentBase: 'http://' + host + ':' + port,
+  quiet: true,
+  noInfo: true,
+  hot: true,
+  inline: true,
+  lazy: false,
+  publicPath: webpackConfig.output.publicPath,
+  headers: {'Access-Control-Allow-Origin': '*'},
+  stats: {colors: true}
+};
+
+var app = new Express();
+
+app.use(require('webpack-dev-middleware')(compiler, serverOptions));
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.listen(port, function onAppListening(err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.info('==> 🚧  Webpack development server listening on port %s', port);
+  }
 });
