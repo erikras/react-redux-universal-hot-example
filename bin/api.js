6350e27eea257d2e50292339190473b89c2eebe4
Michael Kane <mk+github@mk4p.com>
2015-09-17 19:06:29 -0400

    Adding piping hot reloader to api server

--- bin/api.js
+++ bin/api.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+if (process.env.NODE_ENV !== 'production') {
+  if (!require('piping'))({
+    hook: true,
+    ignore: /(\/\.|~$|\.json$)/i
+  })
+}
 require('../server.babel'); // babel registration (runtime transpilation for node)
 require('../api/api');
