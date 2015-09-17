88754964e3a68ace77d8db6f97784bfcb9b4fef4
marclar <marclar@users.noreply.github.com>
2015-09-17 19:13:16 -0400

    Adding return statement

--- bin/api.js
+++ bin/api.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 if (process.env.NODE_ENV !== 'production') {
-  if (!require('piping'))({
+  if (!require('piping')({
     hook: true,
     ignore: /(\/\.|~$|\.json$)/i
-  })
+  })) {
+    return;
+  }
 }
 require('../server.babel'); // babel registration (runtime transpilation for node)
 require('../api/api');
