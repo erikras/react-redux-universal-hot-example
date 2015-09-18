9f5ae818ab24bc26a393e0a5856508b8372b8a70
Nicola Bortignon <info@nicolabortignon.com>
2015-09-18 12:50:39 -0700

    Fixed indentation issues.
    [0]   19:11  error  Expected indentation of 8 space characters but
    found 10  indent
    [0]   20:11  error  Expected indentation of 8 space characters but
    found 10  indent
    [0]   21:9   error  Expected indentation of 6 space characters but
    found 8   indent

--- src/redux/middleware/clientMiddleware.js
+++ src/redux/middleware/clientMiddleware.js
@@ -1,25 +1,31 @@
 export default function clientMiddleware(client) {
-  return ({dispatch, getState}) => {
+  return ({
+    dispatch, getState
+  }) => {
     return next => action => {
       if (typeof action === 'function') {
         return action(dispatch, getState);
       }
 
-      const { promise, types, ...rest } = action;
+      const {
+        promise, types, ...rest
+      } = action;
       if (!promise) {
         return next(action);
       }
 
       const [REQUEST, SUCCESS, FAILURE] = types;
-      next({...rest, type: REQUEST});
+      next({...rest, type: REQUEST
+      });
       return promise(client).then(
-        (result) => next({...rest, result, type: SUCCESS}),
-        (error) => next({...rest, error, type: FAILURE})
-      ).catch((error)=> {
-          console.error('MIDDLEWARE ERROR:', error);
-          next({...rest, error, type: FAILURE});
-        }
-      );
+        (result) => next({...rest, result, type: SUCCESS
+        }), (error) => next({...rest, error, type: FAILURE
+        })
+      ).catch((error) => {
+        console.error('MIDDLEWARE ERROR:', error);
+        next({...rest, error, type: FAILURE
+        });
+      });
     };
   };
 }
