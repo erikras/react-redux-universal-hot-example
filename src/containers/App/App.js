36bcacc25dd90ee567bee1ad5cc9a58169cadfd4
Nicola Bortignon <info@nicolabortignon.com>
2015-09-19 14:03:33 -0700

    Added Fonts

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -89,7 +89,7 @@ export default class App extends Component {
 
   render() {
     const {user} = this.props;
-    const styles = require('./App.scss');
+    const styles = require('./App.less');
     return (
       <div className={styles.app}>
         <DocumentMeta {...meta}/>
