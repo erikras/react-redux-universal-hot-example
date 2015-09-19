8e1c1765b0dfd0218dff10ff41b3922d4737d197
Nicola Bortignon <info@nicolabortignon.com>
2015-09-19 15:04:19 -0700

    Revert "Fixed indentation issues."
    This reverts commit 9f5ae818ab24bc26a393e0a5856508b8372b8a70.

--- src/redux/middleware/clientMiddleware.js
+++ src/redux/middleware/clientMiddleware.js
@@ -1,31 +1,25 @@
 export default function clientMiddleware(client) {
-  return ({
-    dispatch, getState
-  }) => {
+  return ({dispatch, getState}) => {
     return next => action => {
       if (typeof action === 'function') {
         return action(dispatch, getState);
       }
 
-      const {
-        promise, types, ...rest
-      } = action;
+      const { promise, types, ...rest } = action;
       if (!promise) {
         return next(action);
       }
 
       const [REQUEST, SUCCESS, FAILURE] = types;
-      next({...rest, type: REQUEST
-      });
+      next({...rest, type: REQUEST});
       return promise(client).then(
-        (result) => next({...rest, result, type: SUCCESS
-        }), (error) => next({...rest, error, type: FAILURE
-        })
-      ).catch((error) => {
-        console.error('MIDDLEWARE ERROR:', error);
-        next({...rest, error, type: FAILURE
-        });
-      });
+        (result) => next({...rest, result, type: SUCCESS}),
+        (error) => next({...rest, error, type: FAILURE})
+      ).catch((error)=> {
+          console.error('MIDDLEWARE ERROR:', error);
+          next({...rest, error, type: FAILURE});
+        }
+      );
     };
   };
 }
