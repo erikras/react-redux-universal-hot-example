ec61d8a3612da3a945257b37ace0d700c6da86ef
stevo <stevoland@gmail.com>
2015-10-09 17:15:29 +0100

    Fix getStatusFromRoutes properly

--- src/helpers/getStatusFromRoutes.js
+++ src/helpers/getStatusFromRoutes.js
@@ -2,16 +2,8 @@
  * Return the status code from the last matched route with a status property.
  *
  * @param matchedRoutes
- * @returns {Number|undefined}
+ * @returns {Number|null}
  */
 export default (matchedRoutes) => {
-  return matchedRoutes.reduce((prev, cur) => {
-    if (cur && cur.status) {
-      return cur.status;
-    }
-
-    if (prev && prev.status) {
-      return prev.status;
-    }
-  });
+  return matchedRoutes.reduce((prev, cur) => cur.status || prev, null);
 };
