98fcc721ad7dd8552b95ff1e65aabb6fe724ffd5
Dan Schuman <danschuman@gmail.com>
2015-10-23 18:20:45 -0700

    Add charSet to HTML link/script tags
    Omitting these can cause some realllly fun bugs if you decide to use Electron =)

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -33,13 +33,13 @@ export default class Html extends Component {
           {/* styles (will be present only in production with webpack extract text plugin) */}
           {Object.keys(assets.styles).map((style, key) =>
             <link href={assets.styles[style]} key={key} media="screen, projection"
-                  rel="stylesheet" type="text/css"/>
+                  rel="stylesheet" type="text/css" charSet="UTF-8"/>
           )}
         </head>
         <body>
           <div id="content" dangerouslySetInnerHTML={{__html: content}}/>
-          <script dangerouslySetInnerHTML={{__html: `window.__data=${serialize(store.getState())};`}} />
-          <script src={assets.javascript.main}/>
+          <script dangerouslySetInnerHTML={{__html: `window.__data=${serialize(store.getState())};`}} charSet="UTF-8"/>
+          <script src={assets.javascript.main} charSet="UTF-8"/>
         </body>
       </html>
     );
