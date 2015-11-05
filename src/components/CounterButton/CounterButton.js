357800e74f15b0e9ebcc4944d991847ba073f916
Samuel Loup <loup@epsitec.ch>
2015-11-05 10:37:52 +0100

    Change CounterButton style

--- src/components/CounterButton/CounterButton.js
+++ src/components/CounterButton/CounterButton.js
@@ -2,29 +2,27 @@ import React, {Component, PropTypes} from 'react';
 import {connectMultireducer} from 'multireducer';
 import {increment} from 'redux/modules/counter';
 
+import {connectTheme} from 'redux-theme';
+
+
 @connectMultireducer(
   state => ({count: state.count}),
   {increment})
+@connectTheme
 export default class CounterButton extends Component {
   static propTypes = {
     count: PropTypes.number,
     increment: PropTypes.func.isRequired,
-    className: PropTypes.string
-  }
-
-  props = {
-    className: ''
+    styles: PropTypes.object,
+    kind: PropTypes.string
   }
 
   render() {
-    const {count, increment} = this.props; // eslint-disable-line no-shadow
-    let {className} = this.props;
-    className += ' btn btn-default';
+    const {count, increment, styles, kind} = this.props; // eslint-disable-line no-shadow
     return (
-      <button className={className} onClick={increment}>
+      <button style={[styles.base, styles[kind]]} onClick={increment}>
         You have clicked me {count} time{count === 1 ? '' : 's'}.
       </button>
     );
   }
 }
-
