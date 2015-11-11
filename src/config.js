fbfd9835c2fe2dbc95438ae5977657757f864320
Nils Petersohn <nils.petersohn.extern@zalando.de>
2015-11-11 12:27:18 +0100

    centralize host configuration

--- src/config.js
+++ src/config.js
@@ -10,7 +10,9 @@ const environment = {
 }[process.env.NODE_ENV || 'development'];
 
 module.exports = Object.assign({
+  host: process.env.HOST || 'localhost',
   port: process.env.PORT,
+  apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
     title: 'React Redux Example',
