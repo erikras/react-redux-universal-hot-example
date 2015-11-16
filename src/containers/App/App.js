e0b481ebe02d384e374bb9b53798bf0ab50ba744
Erik Rasmussen <rasmussenerik@gmail.com>
2015-11-16 19:03:42 +0100

    added discord invite to app footer to address #553

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -113,7 +113,7 @@ export default class App extends Component {
           Have questions? Ask for help <a
           href="https://github.com/erikras/react-redux-universal-hot-example/issues"
           target="_blank">on Github</a> or in the <a
-          href="https://discordapp.com/channels/102860784329052160/105739309289623552" target="_blank">#react-redux-universal</a> Discord channel.
+          href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
         </div>
       </div>
     );
