f5ff8426d5591a95436ba6513d1b3e9eb55c1465
Patrick P. Paevere <patrick@paevere.com>
2015-11-17 12:46:11 +1100

    more meta tags

--- src/config.js
+++ src/config.js
@@ -22,18 +22,18 @@ module.exports = Object.assign({
       charSet: 'utf-8',
       property: {
         'og:site_name': 'Explore the Melbourne School of Design',
-        'og:image': '',
+        'og:image': 'http://explore.msd.unimelb.edu.au/explore_msd_logo.jpg',
         'og:locale': 'en_US',
         'og:title': 'Explore the Melbourne School of Design',
         'og:description': 'A living learning building, the Melbourne School of Design sets a new standard for design education in the Asia-Pacific region.',
-        'twitter:card': 'summary',
+        'twitter:card': 'summary_large_image',
         'twitter:site': '@msdsocial',
         'twitter:creator': '@msdsocial',
         'twitter:title': 'Explore the Melbourne School of Design',
         'twitter:description': 'A living learning building, the Melbourne School of Design sets a new standard for design education in the Asia-Pacific region.',
-        'twitter:image': '',
-        'twitter:image:width': '200',
-        'twitter:image:height': '200'
+        'twitter:image': 'http://explore.msd.unimelb.edu.au/explore_msd_logo.jpg',
+        'twitter:image:width': '1200',
+        'twitter:image:height': '630'
       }
     }
   }
--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -31,8 +31,8 @@ export default class Html extends Component {
           {DocumentMeta.renderAsReact()}
           <meta charSet="utf-8" />
           <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
-          <meta name="apple-mobile-web-app-capable" content="yes">
-          <meta name="apple-mobile-web-app-status-bar-style" content="black">
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="react-comment-hack" dangerouslySetInnerHTML={{__html: comment}}></meta>
           {/* styles (will be present only in production with webpack extract text plugin) */}
