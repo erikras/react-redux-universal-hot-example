6fdc8f19b540fef7aa3654640fc9e56e768fa717
Néstor Hernández Ojeda <nesukun@gmail.com>
2015-11-17 00:04:56 +0000

    refactor: remove serve-static explicit dependency as it is included already in express 4+

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ const proxy = httpProxy.createProxyServer({
 app.use(compression());
 app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')));
 
-app.use(require('serve-static')(path.join(__dirname, '..', 'static')));
+app.use(Express.static(path.join(__dirname, '..', 'static')));
 
 // Proxy to API server
 app.use('/api', (req, res) => {
