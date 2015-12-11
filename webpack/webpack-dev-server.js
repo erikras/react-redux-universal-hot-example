d2c833110285b5d28b3d5a18659970a84c55d414
chrisblossom <cbloss@gmail.com>
2015-12-10 19:20:11 -0800

    ExtractTextPlugin to dev, add browser sync and write-file-webpack-plugin for css hot reloading in dev

--- webpack/webpack-dev-server.js
+++ webpack/webpack-dev-server.js
@@ -1,14 +1,16 @@
 var Express = require('express');
+var browserSync = require('browser-sync');
 var webpack = require('webpack');
 
 var config = require('../src/config');
 var webpackConfig = require('./dev.config');
 var compiler = webpack(webpackConfig);
 
-var host = config.host || 'localhost';
-var port = (config.port + 1) || 3001;
+var host = config.host;
+var webpackPort = (config.port + 2) || 3002;
+var browserSyncPort = config.port || 3000;
 var serverOptions = {
-  contentBase: 'http://' + host + ':' + port,
+  contentBase: 'http://' + host + ':' + webpackPort,
   quiet: true,
   noInfo: true,
   hot: true,
@@ -20,14 +22,37 @@ var serverOptions = {
 };
 
 var app = new Express();
+var webpackDevMiddleware = require('webpack-dev-middleware')(compiler, serverOptions);
+var webpackHotMiddleware = require('webpack-hot-middleware')(compiler);
+app.use(webpackDevMiddleware);
+app.use(webpackHotMiddleware);
 
-app.use(require('webpack-dev-middleware')(compiler, serverOptions));
-app.use(require('webpack-hot-middleware')(compiler));
-
-app.listen(port, function onAppListening(err) {
+app.listen(webpackPort, function onAppListening(err) {
   if (err) {
     console.error(err);
   } else {
-    console.info('==> 🚧  Webpack development server listening on port %s', port);
+    console.info('==> 🚧  Webpack development server listening on port %s', webpackPort);
+    browserSync({
+      ui: false,
+      ghostMode: false,
+      online: false,
+      open: false,
+      notify: false,
+      host: host,
+      port: browserSyncPort,
+      xip: false,
+      tunnel: true,
+      proxy: {
+        target: host + ':' + (browserSyncPort + 1),
+        middleware: [
+          webpackDevMiddleware,
+          webpackHotMiddleware
+        ]
+      },
+      files: [
+        './static/dist/*.css'
+      ]
+    });
   }
 });
+
