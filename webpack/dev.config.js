bf436c86c721c0b980c5f18a02ce1502066fcc80
kasselTrankos <alvaro.touzon@gmail.com>
2016-01-08 22:40:15 +0100

    added bootstrap by .scss not by js, without bootstrap-loader

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -51,7 +51,7 @@ module.exports = {
   entry: {
     'main': [
       'webpack-hot-middleware/client?path=http://' + host + ':' + port + '/__webpack_hmr',
-      'bootstrap-sass!./src/theme/bootstrap.config.js',
+      '!style!css!sass!./src/theme/bootstrap.config.scss',
       'font-awesome-webpack!./src/theme/font-awesome.config.js',
       './src/client.js'
     ]
