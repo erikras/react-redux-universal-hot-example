05fe6f41eceb1395014c296b1eb309c70b329913
Zader Zheng <yumaoshu@gmail.com>
2016-03-03 00:26:33 +0800

    convert webpack-dev-server port to number

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -6,7 +6,7 @@ var path = require('path');
 var webpack = require('webpack');
 var assetsPath = path.resolve(__dirname, '../static/dist');
 var host = (process.env.HOST || 'localhost');
-var port = parseInt(process.env.PORT) + 1 || 3001;
+var port = (+process.env.PORT + 1) || 3001;
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
 var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
