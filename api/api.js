32c4a4951f04e01198fa2a6943420b9e8e3506c4
Kenneth Truong <kenneth.e.truong@gmail.com>
2016-03-04 12:29:30 -0800

    fix(api) correct path in api.js

--- api/api.js
+++ api/api.js
@@ -3,7 +3,7 @@ import session from 'express-session';
 import bodyParser from 'body-parser';
 import config from '../src/config';
 import * as actions from './actions/index';
-import {mapUrl} from 'utils/url.js';
+import {mapUrl} from './utils/url.js';
 import PrettyError from 'pretty-error';
 import http from 'http';
 import SocketIo from 'socket.io';
