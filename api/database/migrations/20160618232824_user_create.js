a70b2f02d84e73927462c43ae61ec955828582f3
bertho_l <kevin.bertho@gmail.com>
2016-06-19 00:05:18 +0200

    lint

--- api/database/migrations/20160618232824_user_create.js
+++ api/database/migrations/20160618232824_user_create.js
@@ -1,16 +1,12 @@
-exports.up = (knex, Promise) => {
-  return Promise.all([
-    knex.schema.createTableIfNotExists('user', table => {
-      table.increments('id').primary();
-      table.string('email').unique();
-      table.string('password');
-      table.timestamps();
-    })
-  ])
-};
+exports.up = (knex, Promise) => Promise.all([
+  knex.schema.createTableIfNotExists('user', table => {
+    table.increments('id').primary();
+    table.string('email').unique();
+    table.string('password');
+    table.timestamps();
+  })
+]);
 
-exports.down = (knex, Promise) => {
-  return Promise.all([
-    knex.schema.dropTableIfExists('user')
-  ])
-};
+exports.down = (knex, Promise) => Promise.all([
+  knex.schema.dropTableIfExists('user')
+]);
