ba6a0e9c4d4cf9864ff9c3d484caeb87e0036316
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2016-08-15 20:58:03 +0200

    Fix validate hook

--- api/hooks/index.js
+++ api/hooks/index.js
@@ -2,11 +2,13 @@ import { createValidatorPromise as validator } from '../utils/validation';
 import errors from 'feathers-errors';
 
 export function validateHook(schema) {
-  return hook => validator(schema, { service: this, hook })(hook.data)
-    .then(() => hook)
-    .catch(errorsValidation => {
-      if (Object.keys(errorsValidation).length) {
-        throw new errors.BadRequest('Validation failed', errorsValidation);
-      }
-    });
+  return function(hook) {
+    return validator(schema, { service: this, hook })(hook.data)
+      .then(() => hook)
+      .catch(errorsValidation => {
+        if (Object.keys(errorsValidation).length) {
+          throw new errors.BadRequest('Validation failed', errorsValidation);
+        }
+      });
+  };
 }
