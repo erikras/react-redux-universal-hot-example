536c21c87536adba1fc3c397576ff47c43c54229
komcal <kom.sujautra@gmail.com>
2016-10-02 21:44:53 +0700

    add more input type

--- src/components/SurveyForm/surveyValidation.js
+++ src/components/SurveyForm/surveyValidation.js
@@ -4,6 +4,8 @@ import {createValidator, required, maxLength, email} from 'utils/validation';
 const surveyValidation = createValidator({
   name: [required, maxLength(10)],
   email: [required, email],
-  occupation: maxLength(20) // single rules don't have to be in an array
+  occupation: maxLength(20), // single rules don't have to be in an array
+  address: maxLength(100),
+  blood: [required, maxLength(2)]
 });
 export default memoize(10)(surveyValidation);
